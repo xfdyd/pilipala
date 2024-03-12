efacf6efb1ec8e7da2f06a3c31cd1259719fd20c
xfdyd <73767719+xfdyd@users.noreply.github.com>
2024-03-12 08:24:47 +0800

    Update view.dart

--- lib/pages/live_room/view.dart
+++ lib/pages/live_room/view.dart
@@ -1,11 +1,10 @@
 import 'dart:io';
 
-import 'package:PiliPalaX/utils/storage.dart';
 import 'package:floating/floating.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:PiliPalaX/common/widgets/network_img_layer.dart';
-import 'package:PiliPalaX/plugin/pl_player/index.dart';
+import 'package:pilipala/common/widgets/network_img_layer.dart';
+import 'package:pilipala/plugin/pl_player/index.dart';
 
 import 'controller.dart';
 import 'widgets/bottom_control.dart';
@@ -20,8 +19,6 @@ class LiveRoomPage extends StatefulWidget {
 class _LiveRoomPageState extends State<LiveRoomPage> {
   final LiveRoomController _liveRoomController = Get.put(LiveRoomController());
   PlPlayerController? plPlayerController;
-  late Future? _futureBuilder;
-  late Future? _futureBuilderFuture;
 
   bool isShowCover = true;
   bool isPlay = true;
@@ -30,149 +27,82 @@ class _LiveRoomPageState extends State<LiveRoomPage> {
   @override
   void initState() {
     super.initState();
+    plPlayerController = _liveRoomController.plPlayerController;
+    plPlayerController!.onPlayerStatusChanged.listen(
+      (PlayerStatus status) {
+        if (status == PlayerStatus.playing) {
+          isShowCover = false;
+          setState(() {});
+        }
+      },
+    );
     if (Platform.isAndroid) {
       floating = Floating();
     }
-    videoSourceInit();
-    _futureBuilderFuture = _liveRoomController.queryLiveInfo();
-    plPlayerController!.autoEnterFullscreen();
-  }
-
-  Future<void> videoSourceInit() async {
-    _futureBuilder = _liveRoomController.queryLiveInfoH5();
-    plPlayerController = _liveRoomController.plPlayerController;
   }
 
   @override
   void dispose() {
-    floating?.dispose();
     plPlayerController!.dispose();
+    if (floating != null) {
+      floating!.dispose();
+    }
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    Widget videoPlayerPanel = FutureBuilder(
-      future: _futureBuilderFuture,
-      builder: (BuildContext context, AsyncSnapshot snapshot) {
-        if (snapshot.hasData && snapshot.data['status']) {
-          return PLVideoPlayer(
-            controller: plPlayerController!,
-            bottomControl: BottomControl(
-              controller: plPlayerController,
-              liveRoomCtr: _liveRoomController,
-              floating: floating,
-            ),
-          );
-        } else {
-          return const SizedBox();
-        }
-      },
-    );
-
     Widget childWhenDisabled = Scaffold(
       primary: true,
-      backgroundColor: Colors.black,
-      body: Stack(
-        children: [
-          Positioned(
-            left: 0,
-            right: 0,
-            bottom: 0,
-            child: Opacity(
-              opacity: 0.8,
-              child: Image.asset(
-                'assets/images/live/default_bg.webp',
-                fit: BoxFit.cover,
-                // width: Get.width,
-                // height: Get.height,
-              ),
-            ),
-          ),
-          Obx(
-            () => Positioned(
-              left: 0,
-              right: 0,
-              bottom: 0,
-              child: _liveRoomController
-                              .roomInfoH5.value.roomInfo?.appBackground !=
-                          '' &&
-                      _liveRoomController
-                              .roomInfoH5.value.roomInfo?.appBackground !=
-                          null
-                  ? Opacity(
-                      opacity: 0.8,
-                      child: NetworkImgLayer(
-                        width: Get.width,
-                        height: Get.height,
-                        type: 'bg',
-                        src: _liveRoomController
-                                .roomInfoH5.value.roomInfo?.appBackground ??
-                            '',
-                      ),
-                    )
-                  : const SizedBox(),
-            ),
-          ),
-          Column(
-            children: [
-              AppBar(
-                centerTitle: false,
-                titleSpacing: 0,
-                backgroundColor: Colors.transparent,
-                foregroundColor: Colors.white,
-                toolbarHeight:
-                    MediaQuery.of(context).orientation == Orientation.portrait
-                        ? 56
-                        : 0,
-                title: FutureBuilder(
-                  future: _futureBuilder,
-                  builder: (context, snapshot) {
-                    if (snapshot.data == null) {
-                      return const SizedBox();
-                    }
-                    Map data = snapshot.data as Map;
-                    if (data['status']) {
-                      return Obx(
-                        () => Row(
-                          children: [
-                            NetworkImgLayer(
-                              width: 34,
-                              height: 34,
-                              type: 'avatar',
-                              src: _liveRoomController
-                                  .roomInfoH5.value.anchorInfo!.baseInfo!.face,
-                            ),
-                            const SizedBox(width: 10),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  _liveRoomController.roomInfoH5.value
-                                      .anchorInfo!.baseInfo!.uname!,
-                                  style: const TextStyle(fontSize: 14),
-                                ),
-                                const SizedBox(height: 1),
-                                if (_liveRoomController
-                                        .roomInfoH5.value.watchedShow !=
-                                    null)
-                                  Text(
-                                    _liveRoomController.roomInfoH5.value
-                                            .watchedShow!['text_large'] ??
-                                        '',
-                                    style: const TextStyle(fontSize: 12),
-                                  ),
-                              ],
-                            ),
-                          ],
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(
+          MediaQuery.of(context).orientation == Orientation.portrait ? 56 : 0,
+        ),
+        child: AppBar(
+          centerTitle: false,
+          titleSpacing: 0,
+          title: _liveRoomController.liveItem != null
+              ? Row(
+                  children: [
+                    NetworkImgLayer(
+                      width: 34,
+                      height: 34,
+                      type: 'avatar',
+                      src: _liveRoomController.liveItem.face,
+                    ),
+                    const SizedBox(width: 10),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          _liveRoomController.liveItem.uname,
+                          style: const TextStyle(fontSize: 14),
                         ),
-                      );
-                    } else {
-                      return const SizedBox();
-                    }
-                  },
-                ),
-              ),
+                        const SizedBox(height: 1),
+                        if (_liveRoomController.liveItem.watchedShow != null)
+                          Text(
+                              _liveRoomController
+                                      .liveItem.watchedShow['text_large'] ??
+                                  '',
+                              style: const TextStyle(fontSize: 12)),
+                      ],
+                    ),
+                  ],
+                )
+              : const SizedBox(),
+          // actions: [
+          //   SizedBox(
+          //     height: 34,
+          //     child: ElevatedButton(onPressed: () {}, child: const Text('关注')),
+          //   ),
+          //   const SizedBox(width: 12),
+          // ],
+        ),
+      ),
+      body: Column(
+        children: [
+          Stack(
+            children: [
               PopScope(
                 canPop: plPlayerController?.isFullScreen.value != true,
                 onPopInvoked: (bool didPop) {
@@ -181,7 +111,7 @@ class _LiveRoomPageState extends State<LiveRoomPage> {
                   }
                   if (MediaQuery.of(context).orientation ==
                       Orientation.landscape) {
-                    verticalScreenForTwoSeconds();
+                    verticalScreen();
                   }
                 },
                 child: SizedBox(
@@ -190,19 +120,55 @@ class _LiveRoomPageState extends State<LiveRoomPage> {
                           Orientation.landscape
                       ? Get.size.height
                       : Get.size.width * 9 / 16,
-                  child: videoPlayerPanel,
+                  child: plPlayerController!.videoPlayerController != null
+                      ? PLVideoPlayer(
+                          controller: plPlayerController!,
+                          bottomControl: BottomControl(
+                            controller: plPlayerController,
+                            liveRoomCtr: _liveRoomController,
+                            floating: floating,
+                          ),
+                        )
+                      : const SizedBox(),
                 ),
               ),
+              // if (_liveRoomController.liveItem != null &&
+              //     _liveRoomController.liveItem.cover != null)
+              //   Visibility(
+              //     visible: isShowCover,
+              //     child: Positioned(
+              //       top: 0,
+              //       left: 0,
+              //       right: 0,
+              //       child: NetworkImgLayer(
+              //         type: 'emote',
+              //         src: _liveRoomController.liveItem.cover,
+              //         width: Get.size.width,
+              //         height: videoHeight,
+              //       ),
+              //     ),
+              //   ),
             ],
           ),
         ],
       ),
     );
+    Widget childWhenEnabled = AspectRatio(
+      aspectRatio: 16 / 9,
+      child: plPlayerController!.videoPlayerController != null
+          ? PLVideoPlayer(
+              controller: plPlayerController!,
+              bottomControl: BottomControl(
+                controller: plPlayerController,
+                liveRoomCtr: _liveRoomController,
+              ),
+            )
+          : const SizedBox(),
+    );
     if (Platform.isAndroid) {
       return PiPSwitcher(
         childWhenDisabled: childWhenDisabled,
-        childWhenEnabled: videoPlayerPanel,
-        floating: floating,
+        childWhenEnabled: childWhenEnabled,
       );
     } else {
       return childWhenDisabled;
