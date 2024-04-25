90084883d2b68dd1d0c5f4aa1d7c630f856c0ba2
xfdyd <73767719+xfdyd@users.noreply.github.com>
2024-04-25 17:47:20 +0800

    Update controller.dart

--- lib/plugin/pl_player/controller.dart
+++ lib/plugin/pl_player/controller.dart
@@ -472,7 +472,7 @@ class PlPlayerController {
     // await pp.setProperty("vo", "gpu-next");
     // await pp.setProperty("gpu-context", "android");
     // await pp.setProperty("gpu-api", "opengl");
-    await pp.setProperty("vf framedrop", "yes") 
+    await pp.setProperty("opengl-context", "android")
     await player.setAudioTrack(
       AudioTrack.auto(),
     );
