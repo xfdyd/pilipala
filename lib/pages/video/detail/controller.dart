efc1e0c9c554caea5d4a746a35da0b00dc396f52
xfdyd <73767719+xfdyd@users.noreply.github.com>
2024-04-28 12:37:18 +0800

    Update controller.dart

--- lib/pages/video/detail/controller.dart
+++ lib/pages/video/detail/controller.dart
@@ -272,7 +272,7 @@ class VideoDetailController extends GetxController
       ),
       // 硬解
       enableHA: enableHA.value, 
-      hwdec: enableHA.value ? hwdec.value : null, 
+      hwdec: enableHA.value ?? hwdec.value : null, 
       seekTo: seekToTime ?? defaultST,
         duration: duration ?? data.timeLength == null
           ? null
