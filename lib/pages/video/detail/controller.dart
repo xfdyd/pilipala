9e45d23e9692967515383b8a6bfce92800e8174a
xfdyd <73767719+xfdyd@users.noreply.github.com>
2024-04-29 09:07:03 +0800

    Update controller.dart

--- lib/pages/video/detail/controller.dart
+++ lib/pages/video/detail/controller.dart
@@ -276,7 +276,7 @@ class VideoDetailController extends GetxController
       
         //尝试真正实现竖屏软解
       if (firstVideo.width != null && firstVideo.height != null) {
-        if ((firstVideo.width - firstVideo.height) > 0) {
+        if ((firstVideo.width! - firstVideo.height!) > 0) {
         } else {
           enableHA.value = false;
         }
